d276e3fa1d4416ae4bab8ade81975c1b9d265012
delianapujiarti <delianapujiarti38@gmail.com>
2017-07-11 13:57:15 +0700

    hasil dari deliana view, dan lainnya

--- templatesuplaier/navigasisuplai.php
+++ templatesuplaier/navigasisuplai.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin</title>
-    
+	<title>Logistik Gudang</title>
     <link rel="stylesheet" type="text/css" href="assets/css/admin.css">
     <link href="assets/css/sb-admin-2.css" rel="stylesheet">
     <link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/template.css" rel="stylesheet">
     <link href="js/google-code-prettify/prettify.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="isidata">
+    
 
 </head>
 <body>
 <div id="header">
 			<img class="GAMBAR" src="gambar/logo.jpeg" style="width :100px; height :100px; float: left;">
 				<div id="input"	>
-					<h1 id="page-header" style="text-align:center;margin-top:10px">SISTEM INFORMASI  TOKO BANGUNAN</h1>
+					<h1 id="page-header" style="text-align:center;margin:10px">SISTEM INFORMASI  TOKO BANGUNAN</h1>
 				</div>
 </div>	
 <div id="wrapper">
 	<div id="admin">
-		<h1 style="text-align : center">Halaman Admin</h1>
+		<h1 style="text-align : center">Halaman Logistik Gudang</h1>
 			<a class="keluar" href="loginke.html"><button type="submit" >Keluar</button></a>
 	</div>
