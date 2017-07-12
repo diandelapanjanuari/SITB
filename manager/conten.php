ff8bd515f7122964764180cfc4600f152c3d0971
delianapujiarti <delianapujiarti38@gmail.com>
2017-07-12 15:03:09 +0700

    deli

---
+++ manager/conten.php
@@ -0,0 +1,24 @@
+<table class="tabel" border="2" width="500px" style="text-align: center;">
+						<tr >
+							<td>Judul</td>
+							<td>:</td>
+							<td>Penjualan</td>
+						</tr>
+						<tr>
+							<td>Tanggal</td>
+							<td>:</td>
+							<td>1/1/2017</td>
+						</tr>
+						<tr>
+							<td>Nama Laporan</td>
+							<td>:</td>
+							<td>Laporan Bulanan</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
