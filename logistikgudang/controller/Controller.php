cf6ce44a3bb0727a50a448175f19912bfdb36e35
Dian <dianlestari330@gmail.com>
2017-07-13 16:56:14 +0700

    DIAN LESTARI

--- logistikgudang/controller/Controller.php
+++ logistikgudang/controller/Controller.php
@@ -2,43 +2,11 @@
 /**
 * 
 */
-include_once 'Controller.php';
-
-class Barangmanager extends Controller
+class Controller
 {
-	
-	public function getDaftarBarang()
+	public function defaultFunction()
 	{
-		include_once 'model/Barang.php';
-		$kat  = new Kategori();
-		$daftar_kategori = $kat->getDaftarBarang();
-
-		return $daftar_barang;
-	}
-
-	public function setbarang()
-	{
-		include_once 'model/Barang.php';
-		$mbarang = new Barang();
-		$kode_barang = $_POST['kode'];
-		$nama_barang = $_POST['nama'];
-		$merek_barang = $_POST ['merek'];
-		$satuan = $_POST ['satuan'];
-		$harga = $_POST ['harga'];
-		$stok = $_POST ['stok'];
-		
-		$pesan = $mbarang->(,$kode_barang,$nama_barang,$merek_barang,$satuan,$$harga,$stok);
-
-		return $pesan;
-
-	}
-	private function getDataEditBarang(){
-		
-	}
-	
-	public function MenambahDataBarang(){
-		
+		return true;
 	}
 }
-
- ?>
+?>
